c050c7fc03c93aea69b1f415fdfe1eeb1e4c4087
PratyushSrivastav <pratyushsrivastava30@outlook.com>
2021-05-21 19:37:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var translateButton = document.querySelector("#translate-button");
+
+var translateInput = document.querySelector("#translate-input");
+
+var translateOutput = document.querySelector("#translate-output");
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+
+function getTranslationURL(input)
+{
+    return serverURL + "?" + "text=" +input
+}
+
+function errorHandler(error)
+{
+    console.log("Error! Try again later");
+    alert("Server Over-loaded!Please try again.");
+}
+
+function clickHandler()
+{
+    var inputText = translateInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(Response=> Response.json())
+    .then(json=> {var translatedText= json.contents.translated;
+    translateOutput.innerText=translatedText; })
+    .catch(errorHandler)
+};
+
+translateButton.addEventListener("click", clickHandler)
